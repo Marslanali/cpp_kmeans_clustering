8b4d973d8f521f43dacc5919ce905bd5f397aa9d
marslanali <arslanali800@hotmail.com>
2020-03-28 18:11:39 +0500

    updating existing source files and CMakeLists

--- build/CMakeFiles/kmeans.dir/DependInfo.cmake
+++ build/CMakeFiles/kmeans.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/arslanali/CLionProjects/cpp_kmeans_clustering/main.cpp" "/home/arslanali/CLionProjects/cpp_kmeans_clustering/cmake-build-debug/CMakeFiles/kmeans.dir/main.cpp.o"
+  "/home/arslanali/CLionProjects/cpp_kmeans_clustering/main.cpp" "/home/arslanali/CLionProjects/cpp_kmeans_clustering/build/CMakeFiles/kmeans.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- main.cpp
+++ main.cpp
@@ -13,62 +13,61 @@
 #include <vector>
 
 using namespace mlpack::kmeans;
-using namespace arma;
 namespace plt = matplotlibcpp;
 
 
-int main()
+int main(int argc, char** argv)
 
 {
     /**
      *  Generate random vector using random number generators
      */
-    mat X1 = randu(500,2)*0.75+ones(500,2);
-    mat X2 = randu(500,2)*0.5-ones(500,2);
+    arma::Mat<double> x1 = arma::randu(500,2)*0.75+arma::ones(500,2);
+    arma::Mat<double> x2 = arma::randu(500,2)*0.5-arma::ones(500,2);
 
-    mat X = join_cols(X1,X2);
-    X = X.t();
+    arma::Mat<double> x = join_cols(x1,x2);
+    x = x.t();
 
-    std::cout<<"Data:\n"<<X<<std::endl;
-    std::cout<<X.n_rows<<std::endl;
-    std::cout<<X.n_cols<<std::endl;
+    std::cout<<"Data:\n"<<x<<std::endl;
+    std::cout<<x.n_rows<<std::endl;
+    std::cout<<x.n_cols<<std::endl;
 
-    std::vector<double> random_data_X;
-    std::vector<double> random_data_Y;
+    std::vector<double> random_data_x;
+    std::vector<double> random_data_y;
 
-    for (int i=0; i<X.n_cols; ++i)
+    for (int i=0; i<x.n_cols; ++i)
     {
-        random_data_X.push_back(X(0,i));
-        random_data_Y.push_back(X(1,i));
+        random_data_x.push_back(x(0,i));
+        random_data_y.push_back(x(1,i));
     }
 
     plt::figure();
-    plt::plot(random_data_X,random_data_Y,".");
+    plt::plot(random_data_x,random_data_y,".");
     plt::title("Random Data");
-    plt::save("/home/arslan/CLionProjects/cpp_kmeans_clustering/plots/random_data.jpg");
+    plt::save("../plots/random_data.jpg");
     plt::show();
 
-    mat  data = X;
-    uint nbStates = 2;
+    arma::Mat<double>  data = x;
+    uint nb_states = 2;
 
     const float diag_reg_fact = 1e-4f;
-    // vec timing_sep = linspace<vec>(data(0, 0), data(0, data.n_cols - 1), nbStates + 1);
+    // vec timing_sep = linspace<vec>(data(0, 0), data(0, data.n_cols - 1), nb_states + 1);
 
-    std::vector <mat> Mu;
-    std::vector <mat> Sigma;
-    Col <double> Priors;
+    std::vector <arma::Mat<double>> mu;
+    std::vector <arma::Mat<double>> sigma;
+    arma::Col <double> priors;
 
-    Mu.clear();
-    Sigma.clear();
-    // Priors.clear();
+    mu.clear();
+    sigma.clear();
+    // priors.clear();
     // The dataset we are clustering.
-    // mat data;
+    // arma::Mat<double> data;
     // The number of clusters we are getting.
-    size_t clusters = nbStates;
+    size_t clusters = nb_states;
     // The assignments will be stored in this vector.
-    Row <size_t> assignments;
+    arma::Row <size_t> assignments;
     // The centroids will be stored in this matrix.
-    Mat <double> centroids;
+    arma::Mat <double> centroids;
     // Initialize with the default arguments.
     KMeans<> k;
     k.Cluster(data, clusters, assignments, centroids);
@@ -76,23 +75,23 @@ int main()
     //std::cout<<"Assignments : "<<assignments<<std::endl;
     // std::cout<<"Centroids: "<<centroids<<std::endl;
 
-    uvec idtmp;
-    Col <double> priors (nbStates);
+    arma::Col<arma::uword> idtmp;
+    arma::Col <double> priors_vec (nb_states);
 
-    std::vector<double> data_X1;
-    std::vector<double> data_Y1;
+    std::vector<double> data_x1;
+    std::vector<double> data_y1;
 
-    std::vector<double> data_X2;
-    std::vector<double> data_Y2;
-
-
-    std::vector<double> C1;
-    std::vector<double> C2;
+    std::vector<double> data_x2;
+    std::vector<double> data_y2;
 
 
+    std::vector<double> c1;
+    std::vector<double> c2;
 
-    for (int i = 0; i < nbStates; ++i)
-        Mu.push_back(centroids(span::all,i));
+
+
+    for (int i = 0; i < nb_states; ++i)
+        mu.push_back(centroids(arma::span::all,i));
 
     //std::cout<<"Means:\n"<<centroids<<std::endl;
 
@@ -100,8 +99,8 @@ int main()
     //std::cout<<"SIZE\n"<<idtmp.n_elem<<std::endl;
     for (int i = 0; i < idtmp.n_elem ; ++i) {
         //std::cout<<"DATA is \n"<<data(0,idtmp[i])<<std::endl;
-        data_X1.push_back(data(0,idtmp[i]));
-        data_Y1.push_back(data(1,idtmp[i]));
+        data_x1.push_back(data(0,idtmp[i]));
+        data_y1.push_back(data(1,idtmp[i]));
 
     }
     //std::cout<<"assignments\n"<<assignments<<std::endl;
@@ -112,74 +111,65 @@ int main()
 
     for (int i = 0; i < idtmp.n_elem ; ++i) {
         //std::cout<<"DATA is \n"<<data(0,idtmp[i])<<std::endl;
-        data_X2.push_back(data(0,idtmp[i]));
-        data_Y2.push_back(data(1,idtmp[i]));
+        data_x2.push_back(data(0,idtmp[i]));
+        data_y2.push_back(data(1,idtmp[i]));
 
     }
 
-    for (int i = 0; i < nbStates ; ++i) {
-        C1.push_back(centroids(0,i));
-        C2.push_back(centroids(1,i));
+    for (int i = 0; i < nb_states ; ++i) {
+        c1.push_back(centroids(0,i));
+        c2.push_back(centroids(1,i));
     }
 
 
-    const span index(0,1);
+    const arma::span index(0,1);
 
     plt::figure();
-    plt::plot(data_X1,data_Y1,"r.");
+    plt::plot(data_x1,data_y1,"r.");
     //plt::title("Random Data");
     //plt::show();
 
-    plt::plot(data_X2,data_Y2,"b.");
-    plt::plot(C1,C2,"kx");
+    plt::plot(data_x2,data_y2,"b.");
+    plt::plot(c1,c2,"kx");
     plt::title("Kmeans Clustering");
-    plt::save("/home/arslan/CLionProjects/cpp_kmeans_clustering/plots/kmeans_clustering_lot.jpg");
+    plt::save("../plots/kmeans_clustering_lot.jpg");
     plt::show();
 
     std::cout<<"Centers\n"<<centroids<<std::endl;
 
 /*
-    for (int i = 0; i < nbStates; ++i)
+    for (int i = 0; i < nb_states; ++i)
     {
-        priors(i) = idtmp.n_elem;
-        // std::cout<<"\n KMEANS Priors: \n "<< priors(i)<<std::endl;
+        priors_vec(i) = idtmp.n_elem;
+        // std::cout<<"\n KMEANS priors: \n "<< priors_vec(i)<<std::endl;
         // std::cout<<"\n Data for specific Index: \n "<<data.cols(idtmp).t()<<std::endl;
-
-        mat sigma = cov(data.cols(idtmp).t());
+        arma::Mat<double> sigma = cov(data.cols(idtmp).t());
         //std::cout<<"Covariances Matrices:\n"<<sigma<<std::endl;
         // Optional regularization term to avoid numerical instability
        //sigma = sigma + eye(2, 2) * diag_reg_fact;
-
-        Sigma.push_back(sigma);
+        sigma.push_back(sigma);
     }
-
-    Priors = priors / sum(priors);
-    // std::cout<<"Overall Priors:\n"<<priors<<std::endl;
-    //std::cout<<"Priors Sum: "<< sum(priors)<<std::endl;
+    priors = priors_vec / sum(priors_vec);
+    // std::cout<<"Overall priors:\n"<<priors_vec<<std::endl;
+    //std::cout<<"priors Sum: "<< sum(priors_vec)<<std::endl;
 */
     /*
     // kmeans clustering testing
-
-    mat X1 = randu(100,2)*0.75+ones(100,2);
-    mat X2 = randu(100,2)*0.5-ones(100,2);
-
-    mat X = join_cols(X1,X2);
-
-    std::cout<<"\n "<<X<<std::endl;
-    std::cout<<X.n_rows<<std::endl;
-    std::cout<<X.n_cols<<std::endl;
-
-    std::vector<double> random_data_X;
-    std::vector<double> random_data_Y;
-
-    for (int i=0; i<X.n_rows; ++i)
+    arma::Mat<double> x1 = randu(100,2)*0.75+ones(100,2);
+    arma::Mat<double> x2 = randu(100,2)*0.5-ones(100,2);
+    arma::Mat<double> x = join_cols(x1,x2);
+    std::cout<<"\n "<<x<<std::endl;
+    std::cout<<x.n_rows<<std::endl;
+    std::cout<<x.n_cols<<std::endl;
+    std::vector<double> random_data_x;
+    std::vector<double> random_data_y;
+    for (int i=0; i<x.n_rows; ++i)
     {
-        random_data_X.push_back(X(i,0));
-        random_data_Y.push_back(X(i,1));
+        random_data_x.push_back(x(i,0));
+        random_data_y.push_back(x(i,1));
     }
-
     plt::figure();
-    plt::plot(random_data_X,random_data_Y,".");
+    plt::plot(random_data_x,random_data_y,".");
     plt::title("Random Data");
     plt::show();
 */
